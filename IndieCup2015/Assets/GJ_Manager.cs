a8791f6a7d2739d31928c36e9d15ed66f1c66768
Jacob Bevilacqua <jacobbev@gmail.com>
2015-07-09 20:15:04 -0400

    Started GameJolt integration

---
+++ IndieCup2015/Assets/GJ_Manager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GJ_Manager : MonoBehaviour {
+    private bool displayedLogin;
+    private bool loginScreenDisplayed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GameJolt.API.Manager.Instance.CurrentUser != null && !displayedLogin)
+        {
+            Debug.Log("Login: " + GameJolt.API.Manager.Instance.CurrentUser.Name);
+            GameJolt.UI.Manager.Instance.QueueNotification("Welcome " + GameJolt.API.Manager.Instance.CurrentUser.Name + "!", GameJolt.API.Manager.Instance.CurrentUser.Avatar);
+            GameObject.Find("Canvas").GetComponent<Animator>().Play("DISPLAY_LOGIN_BUTTON_REVERSE");
+            GameObject.Find("Name").GetComponent<Text>().text = "AS " + GameJolt.API.Manager.Instance.CurrentUser.Name;
+            displayedLogin = true;
+        }   
+        else if(!loginScreenDisplayed)
+        {
+            GameObject.Find("Canvas").GetComponent<Animator>().Play("DISPLAY_LOGIN_BUTTON");
+            loginScreenDisplayed = true;
+        }
+	}
+
+    public void displayLoginScreen()
+    {
+        GameJolt.UI.Manager.Instance.ShowSignIn();
+    }
+
+    public void goToPlayScreen()
+    {
+        Application.LoadLevel(1);
+    }
+}
