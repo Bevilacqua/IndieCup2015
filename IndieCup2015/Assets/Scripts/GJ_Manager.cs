b49b6d22059b00380f23994cb18237ef4b60efd5
Jacob Bevilacqua <jacobbev@gmail.com>
2015-07-12 02:44:43 -0400

    API bug fix

--- IndieCup2015/Assets/Scripts/GJ_Manager.cs
+++ IndieCup2015/Assets/Scripts/GJ_Manager.cs
@@ -35,44 +35,4 @@ public class GJ_Manager : MonoBehaviour {
     {
         GameJolt.UI.Manager.Instance.ShowSignIn();
     }
-
-
-    public void setEasy()
-    {
-        EndGame.difficulty = EndGame.Difficulty.EASY;
-        Game_Manager.MAX_DIFFICULTY = 1f;
-        Game_Manager.MAX_HEALTH = 200;
-    }
-
-    public void setNormal()
-    {
-        EndGame.difficulty = EndGame.Difficulty.NORMAL;
-        Game_Manager.MAX_DIFFICULTY = 1.25f;
-        Game_Manager.MAX_HEALTH = 100;
-    }
-
-    public void setHard()
-    {
-        EndGame.difficulty = EndGame.Difficulty.HARD;
-        Game_Manager.MAX_DIFFICULTY = 1.5f;
-        Game_Manager.MAX_HEALTH = 100;
-    }
-
-    public void setExtreme()
-    {
-        EndGame.difficulty = EndGame.Difficulty.EXTREME;
-        Game_Manager.MAX_DIFFICULTY = 2.5f;
-        Game_Manager.MAX_HEALTH = 75;
-    }
-
-    public void goToPlayScreen()
-    {
-        Application.LoadLevel("default");
-    }
-
-    public void showDifficultySelect()
-    {
-        difficultyScreen.SetActive(true);
-        difficultyScreen.GetComponent<Animator>().Play("SHOW");
-    }
 }
--- IndieCup2015/Assets/Scripts/PlayMenuLoad.cs
+++ IndieCup2015/Assets/Scripts/PlayMenuLoad.cs
@@ -2,15 +2,56 @@
 using System.Collections;
 
 public class PlayMenuLoad : MonoBehaviour {
+    private GameObject difficultyScreen;
 
 	// Use this for initialization
 	void Start () {
         GetComponent<Animator>().Play("MENULOAD");
-        GameObject.Find("DIFFICULTYSELECT").SetActive(false);
+        difficultyScreen = GameObject.Find("DIFFICULTYSELECT");
+        difficultyScreen.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    
 	}
+
+    public void showDifficultySelect()
+    {
+        difficultyScreen.SetActive(true);
+        difficultyScreen.GetComponent<Animator>().Play("SHOW");
+    }
+
+    public void setEasy()
+    {
+        EndGame.difficulty = EndGame.Difficulty.EASY;
+        Game_Manager.MAX_DIFFICULTY = 1f;
+        Game_Manager.MAX_HEALTH = 200;
+    }
+
+    public void setNormal()
+    {
+        EndGame.difficulty = EndGame.Difficulty.NORMAL;
+        Game_Manager.MAX_DIFFICULTY = 1.25f;
+        Game_Manager.MAX_HEALTH = 100;
+    }
+
+    public void setHard()
+    {
+        EndGame.difficulty = EndGame.Difficulty.HARD;
+        Game_Manager.MAX_DIFFICULTY = 1.5f;
+        Game_Manager.MAX_HEALTH = 100;
+    }
+
+    public void setExtreme()
+    {
+        EndGame.difficulty = EndGame.Difficulty.EXTREME;
+        Game_Manager.MAX_DIFFICULTY = 2.5f;
+        Game_Manager.MAX_HEALTH = 75;
+    }
+
+    public void goToPlayScreen()
+    {
+        Application.LoadLevel("default");
+    }
 }
